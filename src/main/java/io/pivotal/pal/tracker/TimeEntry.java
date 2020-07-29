0cccc47acb4b6d320a380ab380e368332991fdbc
Nick Kilpatrick <kilpat22@msu.edu>
2020-07-29 19:23:51 +0000

    removed instructor code I forget about

--- src/main/java/io/pivotal/pal/tracker/TimeEntry.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntry.java
@@ -4,85 +4,7 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 public class TimeEntry {
-    private long id;
-    private long projectId;
-    private long userId;
-    private LocalDate date;
-    private int hours;
 
-
-    public TimeEntry() {
-    }
-
-    public TimeEntry(long id, long projectId, long userId, LocalDate date, int hours) {
-        this.id = id;
-        this.projectId = projectId;
-        this.userId = userId;
-        this.date = date;
-        this.hours = hours;
-    }
-
-    public TimeEntry(long projectId, long userId, LocalDate date, int hours) {
-        this.id = 0;
-        this.projectId = projectId;
-        this.userId = userId;
-        this.date = date;
-        this.hours = hours;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public long getProjectId() {
-        return projectId;
-    }
-
-    public long getUserId() {
-        return userId;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public int getHours() {
-        return hours;
-    }
-
-    @Override
-    public String toString() {
-        return "TimeEntry{" +
-                "id=" + id +
-                ", projectId=" + projectId +
-                ", userId=" + userId +
-                ", date=" + date +
-                ", hours=" + hours +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        TimeEntry timeEntry = (TimeEntry) o;
-        return id == timeEntry.id &&
-                projectId == timeEntry.projectId &&
-                userId == timeEntry.userId &&
-                hours == timeEntry.hours &&
-                Objects.equals(date, timeEntry.date);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, projectId, userId, date, hours);
-    }
-
-    public TimeEntry withId(long id) {
-        return new TimeEntry(id, projectId, userId, date, hours);
-    }
-}
-/*
     private long id;
     private long projectId;
     private long userId;
@@ -155,5 +77,5 @@ public class TimeEntry {
                 '}';
     }
 
- */
+}
 
